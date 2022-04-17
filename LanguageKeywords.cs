f4486fbb3ffe4c277833ee34921008f77aad3619
noornabidev50@hotmail.com <noornabidev50@hotmail.com>
2022-04-17 22:30:14 +0500

    Add project files.

---
+++ LanguageKeywords.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LexicalAnalyzer
+{
+    internal class LanguageKeywords
+    {
+        private static Dictionary<string, string[]> languageKeywordspair;
+
+        static LanguageKeywords()
+        {
+            languageKeywordspair = new Dictionary<string, string[]>();
+            languageKeywordspair.Add("C#", new string[]
+{
+    "bool", "byte", "sbyte", "short", "ushort", "int", "uint", "long", "ulong", "double", "float", "decimal",
+    "string", "char", "void", "object", "typeof", "sizeof", "null", "true", "false", "if", "else", "while", "for", "foreach", "do", "switch",
+    "case", "default", "lock", "try", "throw", "catch", "finally", "goto", "break", "continue", "return", "public", "private", "internal",
+    "protected", "static", "readonly", "sealed", "const", "fixed", "stackalloc", "volatile", "new", "override", "abstract", "virtual",
+    "event", "extern", "ref", "out", "in", "is", "as", "params", "__arglist", "__makeref", "__reftype", "__refvalue", "this", "base",
+    "namespace", "using", "class", "struct", "interface", "enum", "delegate", "checked", "unchecked", "unsafe", "operator", "implicit", "explicit"
+}
+);
+       
+ }
+
+
+        public static string[] GetLangaugeKeywords(string langauge)
+        {
+            return languageKeywordspair[langauge];
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using System.Linq;
+
+namespace LexicalAnalyzer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string code = File.ReadAllText(@"D:\LexicalAnalyzer\TestFiles\Csharp.txt");
+            string[] words = code.Split(' ');
+            string[] keywords = LanguageKeywords.GetLangaugeKeywords("C#");
+            foreach(var word in words)
+            {
+               if(keywords.Contains(word))
+                {
+                    Console.WriteLine($"{word} is Keyword");
+                }
+            }
+        }
+    }
+}
